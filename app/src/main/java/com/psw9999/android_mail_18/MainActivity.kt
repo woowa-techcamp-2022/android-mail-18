669ef39bbdc355d37290c8464b7220a2be47cfb4
WSP99 <nosod3@naver.com>
2022-07-06 10:14:06 +0900

    First Commit

---
+++ app/src/main/java/com/psw9999/android_mail_18/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.psw9999.android_mail_18
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
